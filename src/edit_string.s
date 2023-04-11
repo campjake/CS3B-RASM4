91dc54faab59e89e2a36c145d8e4e96fe1da9fd2
campjake <campbell.jakelog@gmail.com>
2023-04-10 19:04:50 -0700

    Updated edit_string.s to get list length on its own

--- src/edit_string.s
+++ src/edit_string.s
@@ -9,10 +9,7 @@
 // The function then allows the user to edit the string at the node
 
 // Pre-conditions:
-//	X0 - Contains the index # of the string
 //	X1 - Contains the pointer to the head of the list
-//	X2 - Contains the length of the list
-//	X3 - Contains the size of each node (16 bytes for RASM4)
 //	*** Assumes the last 8 bytes of node are next pointer***
 
 // Post-conditions:
@@ -23,6 +20,7 @@
 	.data
 szNewStrInput:	.skip	512		// New string to get from user
 szIndexAsStr	.skip	21		// Largest number in 64-bit
+szGetIndexPr:	.asciz	"Enter search index: "
 szCurrentStr:	.asciz	"String found! Printing string...\n\n"
 szNoString:		.asciz	"The string was not found\n\n"
 szNewStrPrompt:	.asciz	"Enter edited string below: "
@@ -39,19 +37,43 @@ edit_string:
 	STR		X19, [SP, #-16]!	// Push X19
 	STR		X20, [SP, #-16]!	// Push X20
 	STR		X21, [SP, #-16]!	// Push X21
+	STR		X21, [SP, #-16]!	// Push X22
 	STR		LR, [SP, #-16]!		// Push LR
 
-// Find the node requested by the user	
-	MOV		X19, X0					// Copy index to X19
-	BL		sequential_search_list	// Get the node, if it exists, in X0
-	CMP		X0, #-1					// Make sure it exists
-	BEQ		string_not_found		// Let user know the string isn't there
-	MOV		X20, X0					// Copy node address to X20
+// Get listLength
+	MOV		X19, X1				// Copy head
+	BL		data_count			// X1 = listLength
+	MOV		X20, X1				// Copy list length
 
-// Convert index number to string
-	MOV		X0, X19					// Copy index back to X0
-	LDR		X1,=szIndexAsStr		// Load pointer to index as a string
-	BL		int64asc				// X1 points to indexStr
+// Prompt User for index number
+	LDR		X0,=szGetIndexPr	// Load address of prompt
+	BL		putstring			// Print Prompt
+
+// Get Index Number
+	LDR		X0,=szIndexAsStr	// Load address of buffer
+	MOV		X1, #21				// Size of buffer
+	BL		getstring			// String stored to memory
+
+	LDR		X0,=chLF			// Load Line Feed
+	BL		putch				// Print Line Feed
+
+	LDR		X0,=chLF			// Load Line Feed
+	BL		putch				// Print Line Feed
+
+// Convert Index to int
+	LDR		szIndexAsStr		// Load address of string
+	BL		ascint64			// X0 = index
+
+// Find the node requested by the user	
+	MOV		X21, X0					// Copy index to X21
+	MOV		X1, X19					// Copy head to X1
+	MOV		X2, X20					// Copy listLength to X2
+	MOV		X3, #16					// Node size in X3
+	BL		sequential_search_list	// Get the node, if it exists, in X0
+
+	CMP		X0, #-1					// Make sure node exists
+	BEQ		string_not_found		// Let user know the string isn't there
+	MOV		X22, X0					// Copy node address to X20
 
 // Print the string
 	LDR		X0,=chTAB				// Load tab
@@ -66,7 +88,7 @@ edit_string:
 	LDR		X0,=szColon				// Load ": "
 	BL		putstring				// Print ": "
 
-	LDR		X0, [X19]				// Load address of string to X0 
+	LDR		X0, [X22]				// Load address of string to X0 
 	BL		putstring				// Print string for user to see
 
 	LDR		X0,=chLF				// Load Line Feed
@@ -90,7 +112,7 @@ edit_string:
 	BL		getstring				// String saved to memory
 
 // Edit the string in the node
-	LDR		X1, [X19]				// Load the node
+	LDR		X1, [X22]				// Load the node
 	LDR		X1, [X1]				// Load the address of the string
 
 	LDR		X0,=szNewStrInput		// Load the address of the new string
@@ -104,6 +126,7 @@ string_not_found:
 
 done:
 	LDR		LR, [SP], #16		// Pop LR
+	STR		X22, [SP, #-16]!	// Push X22
 	LDR		X21, [SP], #16		// Pop X21
 	LDR		X20, [SP], #16		// Pop X20
 	LDR		X19, [SP], #16		// Pop X19
